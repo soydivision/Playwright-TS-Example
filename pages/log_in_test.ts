b0c4ad2ee9c93dcc88bca81e690642df23fa56df
Bagzhan Sadvakassov <bagzhan.sadvakassov@dnapayments.com>
2024-12-08 18:16:51 +0500

    refactor

--- pages/log_in_test.ts
+++ pages/log_in_test.ts
@@ -4,7 +4,6 @@ import { PASSWORD, USERNAME } from "../locators/locators";
 export class homepage {
   readonly page: Page;
 
-
   constructor(page: Page) {
     this.page = page;
   }
@@ -13,12 +12,10 @@ export class homepage {
     await this.page.goto("https://www.saucedemo.com/");
   }
 
-
   async fillUserNameAndPassword(name: string, pass: string) {
     await this.page.locator(USERNAME).fill(name)
     await this.page.locator(PASSWORD).fill(pass)
     await this.page.keyboard.press('Enter');
   }
-
 }
 
